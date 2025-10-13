72b28a186c3b51b507bf7fa82052b034785ccc71
vedanthanekar45 <vedanthanekar45@gmail.com>
2025-10-13 10:45:24 +0530

    chore: make db a package

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
+class Directors(str, Enum):
+    Christopher_Nolan = "nolan"
+    David_Fincher = "fincher"
+    Quentin_Tarantino = "tarantino"
+
 @app.get("/")
-async def read_root():
+async def root():
     return {"message": "Hello, World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/movies/{movie_id}")
+async def read_item(movie_id: int):
+    return {"item_id": movie_id}
+
+@app.get("/directors/{director_name}")
+async def read_director(director_name: Directors):
+    if director_name is Directors.Christopher_Nolan:
+        return {"director_name": director_name, "message": "Time is always one of the cast!"}
+    
+    if director_name == "fincher":
+        return {"director_name": director_name, "message": "Mystery is always one of the cast!"}
+    
+    return {"director_name": director_name, "message": "Have some residuals"}
