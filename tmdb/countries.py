2e5a701de9b1d2f17bce42aa9f3d2acd65734d19
vedanthanekar45 <vedanthanekar45@gmail.com>
2025-11-06 10:13:24 +0530

    script to seed countries

---
+++ tmdb/countries.py
@@ -0,0 +1,45 @@
+import os
+import requests
+from dotenv import load_dotenv
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from db.movie_models import Country, engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+url = "https://api.themoviedb.org/3/configuration/countries"
+params = {
+    "limit": 10,
+    "offset": 0
+}
+
+headers = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {os.getenv("TMDB_ACCESS_TOKEN")}"
+}
+
+response = requests.get(url, headers=headers)
+data = response.json()
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Inserting Data into the database
+try:
+    for country_data in data:
+        new_country = Country(
+            iso_3166_1 = country_data['iso_3166_1'],
+            country_name = country_data['english_name']
+        )
+        session.add(new_country)
+    
+    session.commit()
+    print("Successfully added countries to the database!")
+
+except Exception as e:
+    print(f"An error occured: {e}")
+
+finally:
+    session.close()
