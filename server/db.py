55690a35c4db8fb937f9c1988616214866be8bc8
vedanthanekar45 <vedanthanekar45@gmail.com>
2026-01-05 12:42:47 +0530

    chore: define database

--- server/db.py
+++ server/db.py
@@ -5,7 +5,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
 
 if DATABASE_URL is None:
     raise ValueError("DATABASE_URL environment variable not set")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    """Dependency that provides a database session."""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
